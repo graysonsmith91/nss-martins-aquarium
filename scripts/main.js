b34bd97ef17d4c1b861aa94cfc8ceb99a22fd64f
Grayson Smith <graysonsmith91@gmail.com>
2022-12-12 10:27:06 -0600

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+import { getFish } from './database.js'
+import { FishList } from './FishList.js'
+
+/*
+const allFish = getFish()
+
+
+for (const fish of allFish) {
+    console.log(fish)
+}
+*/
+
+const parentHTMLElement = document.querySelector(".fish-list")
+
+parentHTMLElement.innerHTML = FishList()
